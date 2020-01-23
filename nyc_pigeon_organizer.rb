6c5e09cb7f8891229736951bfb989a3c582db95f
Phillip Henry III <philtrader71@gmail.com>
2020-01-23 16:24:16 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -5,10 +5,7 @@ def nyc_pigeon_organizer(data)
     value.each do |new_value, names|
       names.each do |name|
 
-        if !new_hash[name]
-          new_hash[name] = {}
-        end
-
+        
         if !new_hash[name][key]
           new_hash[name][key] = []
         end
